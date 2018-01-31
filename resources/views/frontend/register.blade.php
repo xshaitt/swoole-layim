602204b4b494acc8ee7d8f510b1b68857097c973
xshaitt <xshaitt@gmail.com>
2018-01-31 14:32:07 +0800

    前端添加注册

--- resources/views/frontend/register.blade.php
+++ resources/views/frontend/register.blade.php
@@ -15,18 +15,18 @@
 </head>
 <h1>创建用户开始聊天吧</h1>
 <body>
-<form class="layui-form" method="post"> <!-- 提示：如果你不想用form，你可以换成div等任何一个普通元素 -->
+<form class="layui-form" method="post" onsubmit="return saveUser()"> <!-- 提示：如果你不想用form，你可以换成div等任何一个普通元素 -->
     <div class="layui-form-item">
         <label class="layui-form-label">手机号</label>
         <div class="layui-input-block">
-            <input type="text" name="phone" placeholder="手机号" autocomplete="off" class="layui-input"
+            <input type="text" name="phone" placeholder="手机号" autocomplete="off" class="layui-input input-phone"
                    lay-verify="required|phone|number">
         </div>
     </div>
     <div class="layui-form-item">
         <label class="layui-form-label">昵称</label>
         <div class="layui-input-block">
-            <input type="text" name="nickname" placeholder="昵称" autocomplete="off" class="layui-input"
+            <input type="text" name="nickname" placeholder="昵称" autocomplete="off" class="layui-input input-nickname"
                    lay-verify="required">
         </div>
     </div>
@@ -39,12 +39,19 @@
     <!-- 更多表单结构排版请移步文档左侧【页面元素-表单】一项阅览 -->
 </form>
 <script src="{{asset('/layui/layui.js')}}"></script>
+<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
 <script>
     layui.use('form', function () {
         var form = layui.form;
 
         //各种基于事件的操作，下面会有进一步介绍
     });
+
+    function saveUser() {
+        localStorage.imUserPhone = $('.input-phone').val();
+        localStorage.imUserNickname = $('.input-nickname').val();
+        return true;
+    }
 </script>
 </body>
 </html>
